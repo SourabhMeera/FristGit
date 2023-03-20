27b9b6d6302c19e9020dd428dda2af79e93e93d3
SourabhMeera <skskathihogforever@gmail.com>
2023-03-20 17:49:18 +0530

    Li tag used

--- Gitstart/dom.js
+++ Gitstart/dom.js
@@ -6,7 +6,7 @@ header.style.borderBottom = 'solid 3px #000';
 
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main');
-header.style = 'color:green';*/
+header.style = 'color:green';
 
 var items = document.getElementsByClassName('list-group-item');
 items[2].style.fontWeight = 'bold';
@@ -17,4 +17,19 @@ for(var i = 0; i < items.length; i++)
     items[i].style.fontWeight = 'bold';
     items[i].style.color = 'red';
 
+}*/
+
+
+
+var li = document.getElementsByTagName('li');
+li[2].textContent = 'Exhausted';
+li[2].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < li.length; i++)
+{
+    li[i].style.fontWeight = 'bold';
+    li[i].style.color = 'yellow';
+
 }
+li[4].style.color = 'green';
