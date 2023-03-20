6c58d81395cbdba64c6f81975083c9a9398c7482
SourabhMeera <skskathihogforever@gmail.com>
2023-03-20 12:59:11 +0530

    item list added

---
+++ Gitstart/dom.js
@@ -0,0 +1,9 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headerTitle);
+headerTitle.textContent = 'TRY';
+header.style.borderBottom = 'solid 3px #000';
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main');
+header.style = 'color:green';
