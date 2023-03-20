8ca38cd10d340832cc9ff82f2d9038b3c0e9ea75
SourabhMeera <skskathihogforever@gmail.com>
2023-03-20 16:55:53 +0530

    elementsbyclassselected

--- Gitstart/dom.js
+++ Gitstart/dom.js
@@ -1,4 +1,4 @@
-var headerTitle = document.getElementById('header-title');
+/*var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main-header');
 //console.log(headerTitle);
 headerTitle.textContent = 'TRY';
@@ -6,4 +6,15 @@ header.style.borderBottom = 'solid 3px #000';
 
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main');
-header.style = 'color:green';
+header.style = 'color:green';*/
+
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+    items[i].style.color = 'red';
+
+}
